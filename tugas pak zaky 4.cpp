cc9e7b1151d5eff2cb72e0ba6528194df25a2a1c
ArulBahtiyar97 <61462098+ArulBahtiyar97@users.noreply.github.com>
2020-03-03 21:51:50 +0700

    Add files via upload

---
+++ tugas pak zaky 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int umur ;
+	cout<< "umur kamu berapa ? ";
+	cin>> umur;
+	
+	if (umur >=17){
+	    cout<< " YA , kamu boleh membuat E-KTP ! :)"<<endl;
+	}
+	else{
+		cout<< " Tidak , kamu belum boleh membuat E-KTP !"<<endl;
+	}	
+}
