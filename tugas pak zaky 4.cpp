11a935cfaf776920c2fb2a573d0815ac82bbc84c
ArulBahtiyar97 <61462098+ArulBahtiyar97@users.noreply.github.com>
2020-03-23 19:26:55 +0700

    Update tugas pak zaky 4.cpp

--- tugas pak zaky 4.cpp
+++ tugas pak zaky 4.cpp
@@ -7,10 +7,11 @@ int main()
 	cout<< "umur kamu berapa ? ";
 	cin>> umur;
 	
-	if (umur >=17){
-	    cout<< " YA , kamu boleh membuat E-KTP ! :)"<<endl;
-	}
-	else{
+	if (umur >=17)
+	    	cout<< " YA , kamu boleh membuat E-KTP ! :)"<<endl;
+	else
 		cout<< " Tidak , kamu belum boleh membuat E-KTP !"<<endl;
-	}	
+
+	system("pause");
+	return 0;
 }
