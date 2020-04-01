302d4c5d9a167072d2116ff56df3479d236d1fbe
ArulBahtiyar97 <61462098+ArulBahtiyar97@users.noreply.github.com>
2020-04-01 20:11:26 +0700

    Add files via upload

---
+++ materi_4_latihan_2_Arul_Bahtiyar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	string x;
+	 do{
+	 	cout<<"Masukkan sandi : ";
+	 	cin>>x;	
+	 }
+	 while(x!="Unpkediri");
+	 { cout<<"Berhasil membuka HP"<<endl;
+	 }
+       
+	system("");
+	return 0;
+}
+
---
+++ materi_4_percobaan_1_Arul_Bahtiyar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	 int i;
+	 
+	 cout<<"manjalankan perulangan tanpa for"<<endl;
+	 cout<<"manjalankan perulangan tanpa for"<<endl;
+	 cout<<"manjalankan perulangan tanpa for"<<endl;
+	 cout<<"manjalankan perulangan tanpa for"<<endl;
+	 cout<<"manjalankan perulangan tanpa for"<<endl;
+	 
+	 for(int i=1;i<5;i++)
+	 {
+		cout<<("menjalankan perulangan dengan for")<<endl;
+		
+	 }
+	 
+       
+	
+
+	system("pause");
+	return 0;
+}
+
